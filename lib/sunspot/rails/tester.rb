40f60bf2021532d29dde3e4f3e0123b822d7e3a1
Jason Rankin <rankin.jason@gmail.com>
2018-03-02 16:13:58 -0500

    Propagate rspec exit status at exit after killing solr

--- lib/sunspot/rails/tester.rb
+++ lib/sunspot/rails/tester.rb
@@ -5,12 +5,12 @@ module Sunspot
   module Rails
     class Tester
       VERSION = '1.0.0'
-      
+
       class << self
         extend Forwardable
-        
+
         attr_accessor :server, :started, :pid
-        
+
         def start_original_sunspot_session
           unless started?
             self.server = Sunspot::Rails::Server.new
@@ -25,27 +25,29 @@ module Sunspot
             give_feedback
           end
         end
-        
+
         def started?
           not server.nil?
         end
-        
+
         def kill_at_exit
-          at_exit {
-            Process.kill('TERM', pid)
-            p = port.to_s || "8983"
-            if server.respond_to?(:exec_in_solr_executable_directory)
-              command = ['./solr', 'stop', '-p', p]
-              server.exec_in_solr_executable_directory(command)
-            end
-          }
+          at_exit do
+            orig_status = $!.status if $!.is_a?(SystemExit)
+             Process.kill('TERM', pid)
+             p = port.to_s || "8983"
+             if server.respond_to?(:exec_in_solr_executable_directory)
+               command = ['./solr', 'stop', '-p', p]
+               server.exec_in_solr_executable_directory(command)
+             end
+            exit orig_status if orig_status
+          end 
         end
-        
+
         def give_feedback
           puts 'Sunspot server is starting...' while starting
           puts "Sunspot server took #{seconds} seconds to start"
         end
-      
+
         def starting
           sleep(1)
           res = Net::HTTP.get_response(URI.parse(uri))
@@ -53,26 +55,26 @@ module Sunspot
         rescue Errno::ECONNREFUSED
           true
         end
-        
+
         def seconds
           '%.2f' % (Time.now - started)
         end
-      
+
         def uri
           "http://#{hostname}:#{port}#{path}/ping"
         end
-        
+
         def_delegators :configuration, :hostname, :port, :path
-      
+
         def configuration
           server.send(:configuration)
         end
-        
+
         def clear
           self.server = nil
         end
       end
-      
+
     end
   end
 end
